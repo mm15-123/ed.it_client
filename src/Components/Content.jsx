58843fe27b547ce067a26b200fb02a468f90168d
HadarShift <57561371+HadarShift@users.noreply.github.com>
2020-04-24 18:59:09 +0300

    commit

--- src/Components/Content.jsx
+++ src/Components/Content.jsx
@@ -11,6 +11,8 @@ import axios from 'axios';
 import { GlobalContext } from '../Context/GlobalContext';
 import FavoriteOutlinedIcon from '@material-ui/icons/FavoriteOutlined';
 import SystemUpdateAltOutlinedIcon from '@material-ui/icons/SystemUpdateAltOutlined';
+import TextareaAutosize from '@material-ui/core/TextareaAutosize';
+import { Route, Link, NavLink,Switch } from 'react-router-dom';
 
 const Local = true;
 
@@ -34,106 +36,106 @@ const settings = {
     slidesToScroll: 1
 };
 
-let update=false;//מונע עדכון לייק בעת עליית מסך 
+let update = false;//מונע עדכון לייק בעת עליית מסך 
 
 const Content = (props) => {
     const [ContentToShow, setContentToShow] = useState('')
     const [PagesSourceList, setPagesSourceList] = useState([])
+    const [Comments, setComments] = useState([])
     const [GlobalUser, setGlobalUser, UrlPath, UrlPathFiles, Server_Url, GlobalContent, setGlobalContent, RememberMe, setRememberMe] = useContext(GlobalContext);
-    const [Tagslist,setTagslist]=useState('')
-    const [URLserver,setURLserver]=useState(`http://proj.ruppin.ac.il/igroup20/prod/api/`)
+    const [Tagslist, setTagslist] = useState('')
+    const [URLserver, setURLserver] = useState(`http://proj.ruppin.ac.il/igroup20/prod/api/`)
     const [Like, setLike] = useState('')
-    const [Download,setDownload]=useState(false)
-  
+    const [Download, setDownload] = useState(false)
+
 
     useEffect(() => {
-        update=false;
-        console.log('content Server_Url ',Server_Url)
-        const ContentApiUrl=`${Server_Url}Content/GetContent/${props.match.params.ContentID}/${GlobalUser.Email.split("@", 1)}`
+        update = false;
+        console.log('content Server_Url ', Server_Url)
+        const ContentApiUrl = `${Server_Url}Content/GetContent/${props.match.params.ContentID}/${GlobalUser.Email.split("@", 1)}`
         //const ContentApiUrl = `http://localhost:55263/api/Content/GetContent/${props.match.params.ContentID}`//go to DB and brings the specific presentation with her likes
         //const SContentApiUrl=`http://proj.ruppin.ac.il/igroup20/prod/api/Content/GetContent/${props.match.params.ContentID}`
-        console.log('ContentApiUrl',ContentApiUrl)
+        console.log('ContentApiUrl', ContentApiUrl)
         axios.get(ContentApiUrl)//משיכת פרטי התוכן בעת עליית הקומפוננטה
             .then(res => {
                 console.log(res.data)
                 setContentToShow(res.data)
+                setComments(res.data.CommentsList)
                 MakePages(res.data) // do nothing, just for console.log() staf
                 const PagesSourceNewList = []
                 for (var i = 1; i <= res.data.PagesNumber; i++) { // fill list with the presentasion slides as pictures
-                    PagesSourceNewList.push(  UrlPathFiles + `${res.data.PathFile.split('.', 1)}_${i}.jpg`)
-                    console.log( UrlPathFiles + `${res.data.PathFile.split('.', 1)}_${i}.jpg`)
-                }               
+                    PagesSourceNewList.push(UrlPathFiles + `${res.data.PathFile.split('.', 1)}_${i}.jpg`)
+                    console.log(UrlPathFiles + `${res.data.PathFile.split('.', 1)}_${i}.jpg`)
+                }
                 setPagesSourceList(PagesSourceNewList)
                 console.log(res.data.TagsContent)
 
-                res.data.TagsContent.map((page, index) =>{
-                    return(
+                res.data.TagsContent.map((page, index) => {
+                    return (
                         <span>#{page}</span>
                     )
-                    
+
                 })
                 //רשימת תגים של התוכן
-               setTagslist(()=>{
-                return(
-                    res.data.TagsContent.map((page, index) =>{
-                        return(
-                            <span>#{page}</span>
-                        )
-                    })
-                )
-               }) 
-               setLike(res.data.LikedByUserWhoWatch) 
-        
+                setTagslist(() => {
+                    return (
+                        res.data.TagsContent.map((page, index) => {
+                            return (
+                                <span>#{page}</span>
+                            )
+                        })
+                    )
+                })
+                setLike(res.data.LikedByUserWhoWatch)
+
             })
 
-            //עדכון ניקוד עבור המשתמש
-            axios.put(`${Server_Url}User/UpdateScore/${props.match.params.ContentID}/${GlobalUser.Email.split("@", 1)}/watch`)
-            .then(res=>console.log('hey'))
+        //עדכון ניקוד עבור המשתמש
+        axios.put(`${Server_Url}User/UpdateScore/${props.match.params.ContentID}/${GlobalUser.Email.split("@", 1)}/watch`)
+            .then(res => console.log('hey'))
 
     }, [])
 
-   //משתמש עשה לייק/הוריד את הלייק
-   useEffect(() => {
-        let mode=''
-        if(Like)
-        {
-            console.log("like ",Like)
-            mode='like'
+    //משתמש עשה לייק/הוריד את הלייק
+    useEffect(() => {
+        let mode = ''
+        if (Like) {
+            console.log("like ", Like)
+            mode = 'like'
         }
-        else
-        {
-            console.log("like ",Like)
-            mode='unlike'
+        else {
+            console.log("like ", Like)
+            mode = 'unlike'
         }
         //עדכון ניקוד עבור המשתמש
-        if(update){
+        if (update) {
             axios.put(`${Server_Url}User/UpdateScore/${props.match.params.ContentID}/${GlobalUser.Email.split("@", 1)}/${mode}`)
-            .then(res=>console.log('hey'))
+                .then(res => console.log('hey'))
         }
-    
-      }, [Like])
-      //נלחץ כפתור לייק
-      const LikedClicked=()=>{
-        console.log("like ",Like)
+
+    }, [Like])
+    //נלחץ כפתור לייק
+    const LikedClicked = () => {
+        console.log("like ", Like)
         setLike(!Like)
-        update=true;//שחרור עדכון לייק בדטהבייס
-      }
+        update = true;//שחרור עדכון לייק בדטהבייס
+    }
 
     //נלחץ על כפתור הורדת מצגת
     const ButtonDownloadClicked = () => {
-        console.log('Download clicked',Download)
-        setDownload(true)          
-      }
-      useEffect(() => {     
-        console.log('Download',Download)
-        if(Download){
+        console.log('Download clicked', Download)
+        setDownload(true)
+    }
+    useEffect(() => {
+        console.log('Download', Download)
+        if (Download) {
             axios.put(`${Server_Url}User/UpdateScore/${props.match.params.ContentID}/${GlobalUser.Email.split("@", 1)}/downloaded`)
-            .then(res=>console.log('hey')) 
+                .then(res => console.log('hey'))
         }
         setTimeout(() => {
             setDownload(false);
-          }, 3000);
-      }, [Download])
+        }, 3000);
+    }, [Download])
 
 
     console.log(props)
@@ -163,50 +165,75 @@ const Content = (props) => {
                                         <img className="picContent border-button" src={page} alt='loading' />
                                         {console.log(page)}
                                     </Page>
-                            
+
                                 </div>)
                         }
 
 
                     </Slider>
-               <div className='TitlesContent' >
+                    <div className='TitlesContent' >
                         <div>
                             <br></br>
                             <br></br>
                             <br></br>
 
-                            <h1>{ContentToShow.ContentName}</h1>
-                            <h2>{ContentToShow.Description}</h2>
+                            <h1 style={{ fontSize: '4.0rem' ,fontWeight: 'bolder'}}>{ContentToShow.ContentName}</h1>
+                            <h4>{ContentToShow.Description}</h4>
                             <h6>{ContentToShow.Likes} משתמשים אהבו את המצגת</h6>
                             {/* <span>#{ContentToShow.TagsContent[0]}</span><span>#{ContentToShow.TagsContent[1]} </span><span>#{ContentToShow.TagsContent[2]} </span> */}
                             {Tagslist}
                             <div>
-                                ______________________________________________________________________________
+                                _______________________________________________________________________________________________
                             <table>
                                     <tr>
-                                        <td>              <Avatar alt="Remy Sharp" src={ UrlPath + `${ContentToShow.UserPic}`} className='inline' />
+                                        <td>              <Avatar alt="Remy Sharp" src={UrlPath + `${ContentToShow.UserPic}`} className='inline' />
                                             {console.log('img path ', process.env.PUBLIC_URL + '/uploadedPicturesPub/Shiftan92.jpg')}
-                                            {console.log('avatar Path ',  UrlPath + `${ContentToShow.UserPic}`)}
+                                            {console.log('avatar Path ', UrlPath + `${ContentToShow.UserPic}`)}
                                         </td>
-                                        <td><h3>{ContentToShow.ByUser}  </h3>
+                                        <td><h5>{ContentToShow.ByUser}  </h5>
                                         </td>
                                     </tr>
 
                                 </table>
-                            ______________________________________________________________________________
+                                _______________________________________________________________________________________________
                         </div>
                             <div>
                                 <div className='buttonDownload'>
-                                <Button onClick={ButtonDownloadClicked }><SystemUpdateAltOutlinedIcon style={{fontSize: '2.5rem' }}/>
-                                </Button>
-                                <span>  הורדת המצגת</span>
+                                    <Button onClick={ButtonDownloadClicked}><SystemUpdateAltOutlinedIcon style={{ fontSize: '2.5rem' }} />
+                                    </Button>
+                                    <span>  הורדת המצגת</span>
                                 </div>
-                                <div className='likeDiv'> 
-                                <FavoriteOutlinedIcon className='like' onClick={LikedClicked} style={{ color: Like ? 'red' : 'black', fontSize: '2.5rem' }}></FavoriteOutlinedIcon>
-                                <span>אהבתי</span>      
-                                </div> 
-                                {Download && <iframe src={UrlPathFiles+ContentToShow.PathFile}  allowfullscreen frameborder="0" scrolling="no" > </iframe>}
+                                <div className='likeDiv'>
+                                    <FavoriteOutlinedIcon className='like' onClick={LikedClicked} style={{ color: Like ? 'red' : 'black', fontSize: '2.5rem' }}></FavoriteOutlinedIcon>
+                                    <span>אהבתי</span>
+                                </div>
+                                {Download && <iframe src={UrlPathFiles + ContentToShow.PathFile} allowfullscreen frameborder="0" scrolling="no" > </iframe>}
+                            </div>
+                        <div className='commentDiv'>
+                            <div>
+                                <br></br>
+                                <TextareaAutosize className='commentbox' aria-label=" maximum height" rowsMin={3} rowsMax={4} placeholder="כתוב תגובה" />
+                                <input type='button' className='btnComment' value='הוסף תגובה'></input>
+    
+                            </div>
 
+                            <div className='comment'>
+
+                                { Comments!=null &&
+
+                                    Comments.map((commentObj, index) =>
+                                        <div>
+                                            <div className="commentDesc">  
+                                            <Link to='/User'>     
+                                                    <span>{commentObj.NameWhoCommented}  {commentObj.PublishedDate}</span>                                              
+                                            </Link>  
+                                            </div>
+                                            <TextareaAutosize className='commentbox' aria-label=" maximum height" rowsMin={3} rowsMax={4} defaultValue={commentObj.Comment} disabled />
+
+
+                                        </div>)
+                                }
+                            </div>
                             </div>
                         </div>
 
